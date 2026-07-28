6fac7766b27aba3856441e5f1cd804c73db01880
AlexAp-007 <ale.balto300@gmail.com>
2026-07-28 07:58:19 -0600

    Add files via upload

--- act1.py
+++ act1.py
@@ -1,8 +1,43 @@
 import pandas as pd
-datos = pd.read_csv("estudiantes.csv")
-columnas = ["Edad", "Tareas", "Horas_Estudio", "Calificacion_Final"]
-for columna in columnas:
-    print(f"\n--- {columna} ---")
-    print("Rango:", datos[columna].max() - datos[columna].min())
-    print("Varianza:", datos[columna].var())
-    print("Desviación estándar:", datos[columna].std())
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+
+# Leer archivo
+df = pd.read_csv("dataset_clientes.csv")
+
+# Seleccionar únicamente variables numéricas
+X = df[[
+    "Edad",
+    "Compras_Mensuales",
+    "Gasto_Mensual_MXN",
+    "Visitas_Web_Mensuales",
+    "Dias_Desde_Ultima_Compra",
+    "Tiempo_Sesion_Min",
+    "Clicks_Promedio"
+]]
+
+# Limpieza de datos
+X = X.dropna()
+
+# Escalar los datos
+escalador = StandardScaler()
+X_escalado = escalador.fit_transform(X)
+
+# Modelo K-Means
+modelo = KMeans(
+    n_clusters=3,
+    random_state=42,
+    n_init=10
+)
+
+clusters = modelo.fit_predict(X_escalado)
+
+# Crear nuevo DataFrame con los resultados
+df_limpio = X.copy()
+df_limpio["Cluster"] = clusters
+
+# Mostrar resultados
+print(df_limpio)
+
+print("\nPromedio por cluster:\n")
+print(df_limpio.groupby("Cluster").mean())
